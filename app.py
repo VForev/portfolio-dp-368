f96cd62056a1def0a52fcc09b018ac808bbb64d8
VForev <85714161+VForev@users.noreply.github.com>
2025-02-11 15:37:29 -0800

    udates

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask
-import your_module # this will be your file name; minus the `.py`
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='192.168.1.83', port='8080', debug=True)
+    app.run(host='192.168.1.83', port=8080, debug=True)
